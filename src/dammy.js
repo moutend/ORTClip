3b67b1c38fd6811f967ef0f721506c46c6f529fa
moutend <moutend@gmail.com>
2015-11-17 17:04:14 +0900

    Modify SQL

--- src/dammy.js
+++ src/dammy.js
@@ -28,10 +28,22 @@ function sendSQL(query) {
 };
 
 const SQL = [
-  "INSERT INTO test_table",
+  "INSERT INTO message_table",
   "(hash, isUsing, message, timestamp)",
   "VALUES",
   "('',   FALSE,   '',      clock_timestamp());"
 ].join(' ');
+
+for(let n = 0; n < 10000; n++) {
+  (() => {
+    sendSQL(SQL)
+    .then((result) => {
+      console.log(n);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  })(n);
+}
 
 console.log(SQL);
