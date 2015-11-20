881135bcf1dc07fa2eb901d25feb904b381d1611
moutend <moutend@gmail.com>
2015-11-20 14:18:51 +0900

    Cleanup

--- playground/js/main.js
+++ playground/js/main.js
@@ -1,6 +1,10 @@
-"use strict";
+'use strict';
 
-function zeroFill(n) {
+var _$ = function(querry) {
+  return document.querySelectorAll(querry);
+};
+
+var zeroFill = function(n) {
   return n < 10
     ? "000" + n
     : n < 100
@@ -8,7 +12,152 @@ function zeroFill(n) {
       : n < 1000
         ? "0" + n
         : n;
-}
+};
+
+var scanCode = function(state) {
+  var code = _$('#textarea_code')[0].value
+
+  state.isRefused = false;
+
+  if(code === '') {
+    state.isRefused = true;
+    return state;
+  }
+
+  var id   = code.slice(0, 4);
+  var hash = code.slice(4);
+
+  createWebSocket({
+    mode: 'GET',
+    hash: hash,
+    message: id
+  });
+
+  return state;
+};
+
+var copyToClipboard = function(state) {
+  var t = _$(state.currentScreenName+ ' textarea')[0];
+
+  try {
+    t.select();
+    document.execCommand('copy');
+
+    if ( document.selection ) {
+      document.selection.empty();
+    }
+    else if ( window.getSelection ) {
+      window.getSelection().removeAllRanges();
+    }
+  }
+  catch(error) {
+    handle('#error_screen');
+  }
+
+  state.isRefused = true;
+  return state;
+};
+
+var messageWebSocket = function(options, ws) {
+  return function(event) {
+    var response = JSON.parse(event.data);
+
+    if(response.isOK) {
+      if(options.mode === 'SET' && response.message === '503') {
+        setTimeout(
+          messageWebSocket(options, ws),
+          12000
+        );
+        return;
+      }
+
+      if(options.mode === 'GET') {
+        _$('#textarea_response_message')[0].value = response.message;
+        handle('#get_screen');
+        return;
+      }
+
+      if(options.mode === 'SET') {
+        var text = zeroFill(parseInt(response.message)) + options.hash;
+        var qrcode = new QRCode(_$("#image_qrcode")[0], {
+          text:   text,
+          width:  160,
+          height: 160
+        });
+
+        _$('#textarea_qrcode')[0].value = text;
+        handle('#send_screen');
+        return;
+      }
+    }
+    else {
+      _$('#textarea_error')[0].value = response.message;
+      handle('#error_screen');
+      return;
+    }
+  }
+};
+
+var openWebSocket= function(options, ws) {
+  return function (event) {
+    ws.send(JSON.stringify(options));
+  };
+};
+
+var closeWebSocket = function(options, ws) {
+  _$('#textarea_error')[0].value = 'Connection closed';
+  handle('#error_screen');
+};
+
+var timeoutMessage = function(ws) {
+  _$('#textarea_error')[0].value = 'Connection timed out';
+  handle('#error_screen');
+  return;
+};
+
+var createWebSocket = (function() {
+  var _isOpened = false;
+
+  return function(options) {
+    if(_isOpened === true) {
+      return;
+    }
+
+    _isOpened = true;
+
+    setTimeout(
+      timeoutMessage,
+      24000
+    );
+
+    var protocol  = 'clip-protocol';
+    var uri       = 'wss://ortclip.herokuapp.com';
+    var ws        = new WebSocket(uri, protocol);
+
+    ws.addEventListener('close',   closeWebSocket(options, ws));
+    ws.addEventListener('open',    openWebSocket(options, ws));
+    ws.addEventListener('message', messageWebSocket(options, ws));
+  };
+})();
+
+var sendMessage = function(state) {
+  var message = _$('#textarea_request_message')[0].value
+  var hash    = createHash(8);
+
+  state.isRefused = false;
+  if(message === '') {
+    state.isRefused = true;
+    return state;
+  }
+
+  createWebSocket({
+    mode: 'SET',
+    hash: hash,
+    message: message
+  });
+
+  return state;
+};
 
 function createHash(length) {
   return new Array(length)
@@ -22,135 +171,78 @@ function createHash(length) {
   .join('');
 }
 
-function copyClipboard() {
-  var textarea_recieve = document.querySelector('#textarea_recieve');
-  textarea_recieve.select();
 
-  try {
-    document.execCommand('copy');
-
-    if ( document.selection ) {
-      document.selection.empty();
-    }
-    else if ( window.getSelection ) {
-      window.getSelection().removeAllRanges();
-    }
-  }
-  catch(error) {
-    console.error(error);
-  }
+function clearMessage(state) {
+  _$(state.currentScreenName + ' textarea')[0].value = '';
+  state.isRefused = true;
+  return state;
 }
 
-function sendMessage(options, ws) {
-  return function (event) {
-    if(!options.message && !options.hash) {
-      return;
-    }
-
-    var HASH = options.hash || createHash(8);
-    var MODE = options.hash ? 'GET' : 'SET';
-    var DATA = [MODE, HASH, options.message].join(' ');
-
-    document.querySelector("#hidden_hash").value = HASH;
-    ws.send(DATA);
+var handle = (function() {
+  var _state = {
+    currentScreenName: '#welcome_screen',
+    isRefused: false
   };
-}
+  var _screens = {
+    '#welcome_screen': {
+      '#edit_screen': {},
+      '#scan_screen': {}
+    },
+    '#edit_screen': {
+      '#wait_screen': sendMessage,
+      'clearMessage': clearMessage
+    },
+    '#scan_screen': {
+      '#wait_screen': scanCode
+    },
+    '#wait_screen': {
+      '#send_screen': {},
+      '#get_screen': {},
+      '#error_screen': {}
+    },
+    '#get_screen': {
+      'copy': copyToClipboard
+    },
+    '#send_screen': {
+      'copy': copyToClipboard
+    },
+    '#error_screen': {}
+  };
 
-function recieveMessage(options, ws) {
-  return function(event) {
-    var response = event.data;
+  return function(next_screen_name) {
+    var screen = _screens[_state.currentScreenName][next_screen_name]
 
-    document.querySelector("#editMessage_screen").style.display = "none";
-
-    var id = parseInt(response);
-
-    if(!options.hash) {
-      if(id < 0) {
-        setTimeout(sendMessage(options, ws), 12000);
-      }
-      else {
-        var HASH = document.querySelector("#hidden_hash").value;
-        response = zeroFill(response) + HASH;
-      }
-    }
-
-    if(options.isSender && id < 0) {
-      document.querySelector("#sendingMessage_screen").style.display = "block";
+    if(typeof screen === 'undefined') {
       return;
     }
 
-    if(!options.isSender) {
-      document.querySelector("#textarea_recieve").value = response;
-      document.querySelector("#recieveMessage_screen").style.display = "block";
-      document.querySelector("#recieveMessage_screen").className += " animate-fade-in";
+    if(typeof screen === 'function') {
+      _state = screen(_state);
+    }
+
+    if(_state.isRefused) {
       return;
     }
 
-    var base_url = window.location.href;
-    var uri = base_url.replace(/^.*\/\//, '') + "#" + response;
-    var qr = new QRCode(document.querySelector("#QRCode"), {
-      text:   uri,
-      text:   "http://jindo.dev.naver.com/collie",
-      width:  160,
-      height: 160
-    });
+    var prev = _$(_state.currentScreenName)[0];
+    var next = _$(next_screen_name)[0];
 
-    document.querySelector("#input_uri").value = uri;
-    document.querySelector("#sendingMessage_screen").style.display = "none";
-    document.querySelector("#sendMessage_screen").style.display = "block";
-    document.querySelector("#sendMessage_screen").className += " animate-fade-in";
-  }
-}
+    prev.style.display = "none";
+    prev.className = prev.className.replace(/ animate-fade-in/g, '');
+    next.style.display = "block";
+    next.className += " animate-fade-in";
 
-function foo() {
-  var options = {
-    isSender: true,
-    hash:     null,
-    message:  null
-  }
-
-  if(window.location.hash === "") {
-    options.message = document.querySelector("#textarea_message").value;
-  }
-  else {
-    options.isSender = false;
-    var URI_HASH = window.location.hash.slice(1);
-
-    options.message = parseInt(URI_HASH.slice(0, 4));
-    options.hash    = URI_HASH.slice(4);
-  }
-
-  var websocket_uri = "wss://ortclip.herokuapp.com";
-  var ws = new WebSocket(websocket_uri, 'clip-protocol');
-  ws.addEventListener("open",    sendMessage(options, ws));
-  ws.addEventListener("message", recieveMessage(options, ws));
-}
-
-function clear() {
-  document.querySelector("#textarea_message").value = "";
-}
+    _state.currentScreenName = next_screen_name;
+  };
+})();
 
 window.onload = function() {
-  var button_send  = document.querySelector("#button_send");
-  var button_clear = document.querySelector("#button_clear");
-
-  if(window.location.hash) {
-    setTimeout(function() {
-      foo()
-      document.querySelector("#welcome_screen").style.display = "none";
-      document.querySelector("recieveMessage_screen").style.display = "block";
-      document.querySelector("recieveMessage_screen").className += " animate-fade-in";
-    }, 1000);
-  }
-  else {
-    setTimeout(function() {
-      document.querySelector("#welcome_screen").style.display = "none";
-      document.querySelector("#editMessage_screen").style.display = "block";
-      document.querySelector("#editMessage_screen").className += " animate-fade-in";
-    }, 1000);
-  }
-
-  button_copy.addEventListener("click", copyClipboard);
-  button_send.addEventListener("click", foo);
-  button_clear.addEventListener("click", clear);
+  Array.prototype.map.call(
+    _$('button'),
+    function(button) {
+      button.addEventListener('click', function() {
+        handle(button.className);
+      });
+    }
+  );
 };
